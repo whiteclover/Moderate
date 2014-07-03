dee778181e144b2cd7679d2fb7817d693156b2e3
thomashuang <lyanghwy@gmail.com>
2014-07-03 21:20:42 +0800

    Initialize

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup 
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name = "moderate",
+    version = "0.1",
+    license = 'MIT',
+    description = "A Python Distrubted System",
+    author = 'Thomas Huang',
+    url = 'https://github.com/thomashuang/Moderate',
+    packages = ['moderate'],
+
+    install_requires = ['setuptools',
+                        ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: Proxy Servers',
+        ],
+    long_description=long_description,
+)
